8cd562da67043ad65e395b69671cacea77da54d3
Lucas <71858617+mnttlucas@users.noreply.github.com>
2025-11-21 12:02:17 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define BUFSIZE 256
 
 int main(int argc, char **argv) {
+	char input_buf[BUFSIZE];
+
 	setbuf(stdout, NULL);
-	
+
 	printf("$ ");
+	fgets(input_buf, BUFSIZE, stdin);
+	input_buf[strlen(input_buf) - 1] = '\0';
+	printf("%s: command not found\n", input_buf);
 
 	return(0);
 }

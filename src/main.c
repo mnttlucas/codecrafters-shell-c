f00dc5972923293c98bd8a5c83b8ffc7cca2ba0d
Lucas <71858617+mnttlucas@users.noreply.github.com>
2025-11-21 12:40:41 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,21 +3,36 @@
 #include <string.h>
 
 #define BUFSIZE 256
+#define CMDSIZE 16
+#define NB_BUILTIN_COMMANDS 3
 
+char builtin_commands[NB_BUILTIN_COMMANDS][CMDSIZE] = {"echo", "exit", "type"};
 int run = 1;
 
 int evaluate(char *cmd, char *res)
 {
+	int i;
 	int ret = 1;
 	
 	if(!strncmp(cmd, "echo ", strlen("echo ")))
 	{
-		snprintf(res, BUFSIZE, "%s\n", cmd + 5);
+		snprintf(res, BUFSIZE, "%s\n", cmd + strlen("echo "));
 	}
 	else if(!strncmp(cmd, "exit", strlen("exit")))
 	{
 		ret = 0;
 		run = 0;
+	}
+	else if(!strncmp(cmd, "type ", strlen("type ")))
+	{
+		snprintf(res, BUFSIZE, "%s: not found\n", cmd + strlen("type "));
+		for(i = 0; i < NB_BUILTIN_COMMANDS; i++)
+		{
+			if((!strncmp(builtin_commands[i], cmd + strlen("type "), strlen(builtin_commands[i]))))
+			{
+				snprintf(res, BUFSIZE, "%s is a shell builtin\n", cmd + strlen("type "));
+			}
+		}
 	}
 	else
 	{

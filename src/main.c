38029019f579f45fecf1443a93402b832895ba13
Lucas <71858617+mnttlucas@users.noreply.github.com>
2025-11-21 12:29:10 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -4,22 +4,42 @@
 
 #define BUFSIZE 256
 
+int run = 1;
+
+int evaluate(char *cmd, char *res)
+{
+	if(!strncmp(cmd, "exit", strlen("exit")))
+	{
+		run = 0;
+		return(0);
+	}
+	else
+	{
+		snprintf(res, BUFSIZE, "%s: command not found\n", cmd);
+	}
+
+	return(1);
+}
+
 void repl(void)
 {
+	char input_buf[BUFSIZE];
+	char output_buf[BUFSIZE];
+
 	/* Loop */
-	while(1)
+	while(run)
 	{
-		char input_buf[BUFSIZE];
-	
 		/* Read */
 		printf("$ ");
 		fgets(input_buf, BUFSIZE, stdin);
 		input_buf[strlen(input_buf) - 1] = '\0';
 	
 		/* Eval */
-		
-		/* Print */
-		printf("%s: command not found\n", input_buf);
+		if(evaluate(input_buf, output_buf))
+		{
+			/* Print */
+			printf("%s", output_buf);
+		}
 	}
 }
 
